4ba9136617c277ba115cc8d77366f899b78aaaef
JohnnyJYWu <37619620+JohnnyJYWu@users.noreply.github.com>
2018-05-31 00:08:00 +0800

    音乐播放界面1.0
    布局初步完成，添加了一下矢量图素材

--- app/src/main/java/com/earnest/ui/home/MainActivity.java
+++ app/src/main/java/com/earnest/ui/home/MainActivity.java
@@ -1,15 +1,29 @@
 package com.earnest.ui.home;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.earnest.R;
+import com.earnest.ui.musicPlayer.MusicPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MusicPlayerActivity.class));
+            }
+        });
     }
 }
