e464e16f81da1c334ab4b97ca9208756252ead94
Juderer <1843042966@qq.com>
2018-06-03 11:44:55 +0800

    添加MyUser类，继承了BmobUser

---
+++ app/src/main/java/com/earnest/model/MyUser.java
@@ -0,0 +1,42 @@
+package com.earnest.model;
+
+import cn.bmob.v3.BmobUser;
+
+/**
+ * Created by Administrator on 2018/5/29.
+ */
+
+public class MyUser extends BmobUser {//用户类，添加用户基本属性及对应方法
+    private Boolean gender;     // true，性别女；false，性别男
+    private String nickname;    // 用户昵称
+    private Integer age;        // 用户年龄
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return getUsername()+"\n"+getObjectId()+"\n"+gender+"\n"+age+"\n"+nickname+"\n";
+    }
+}
