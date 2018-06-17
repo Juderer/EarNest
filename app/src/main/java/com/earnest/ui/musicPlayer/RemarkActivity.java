8ceadfbecbef9c8c920608dd58eb2dbca3f097b1
RXting <15301065@bjtu.edu.cn>
2018-06-18 00:28:02 +0800

    修改细节

---
+++ app/src/main/java/com/earnest/ui/musicPlayer/RemarkActivity.java
@@ -0,0 +1,15 @@
+package com.earnest.ui.musicPlayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.earnest.R;
+
+public class RemarkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remark);
+    }
+}
